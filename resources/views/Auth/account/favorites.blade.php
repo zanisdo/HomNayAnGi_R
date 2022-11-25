73b5f5e9242f9a27b894ab5a15c13009124cf4c7
Minh Trường <trminh.truong@outlook.com>
2022-11-26 01:38:23 +0700

    add files

---
+++ resources/views/Auth/account/favorites.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('body')
+    <div class="container">
+        <h1>My Favorites</h1>
+        <div class="display-content-4">
+            {{-- CARD TEMPLATE --}}
+            {{-- <div class="col card" style="width: 18rem;">
+                <img class="card-img-top"
+                    src="https://cdn.discordapp.com/attachments/991705612641640478/1045629661956276225/khobo.jpg"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">`Name`</h5>
+                    <p class="card-text">`Description`</p>
+                    <div class="button-group d-flex justify-content-around">
+                        <button class="custom-button hate-btn"><i class="fa-solid fa-heart-crack"></i></button>
+                        <button class="custom-button already-ate-btn"><i class="fa-solid fa-check"></i></button>
+                    </div>
+                </div>
+            </div> --}}
+            
+        </div>
+    </div>
+@endsection
